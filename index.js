a0c55c7ca39c1020e3862b1ad63415ddeb76934e
Aaronpteer <ocenaaronpeter2001@gmail.com>
2023-02-22 21:08:45 +0300

    Shell

---
+++ index.js
@@ -0,0 +1,28 @@
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+} from "./snake.js";
+
+let lastRenderTime = 0;
+
+const main = (currTime) => {
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+  lastRenderTime = currTime;
+
+  update();
+
+  draw();
+};
+window.requestAnimationFrame(main);
+
+const update = () => {
+  updateSnake();
+};
+
+const draw = () => {
+  drawSnake();
+};
